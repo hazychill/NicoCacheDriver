4652214461023dcfb6a30c1c6814535d99138ad3
Hiroki Murayama <hazychill@gmail.com>
2011-08-18 21:46:32 +0900

    スレッド番号指定のURLが取得できなくなっていた

--- NicoCacheDriver/OnelineVideoInfo.cs
+++ NicoCacheDriver/OnelineVideoInfo.cs
@@ -7,8 +7,8 @@ using System.Diagnostics.Contracts;
 
 namespace Hazychill.NicoCacheDriver {
     struct OnelineVideoInfo {
-        // ^(?<beforeComment>\s*(?:http://www\.nicovideo\.jp/watch/)?(?<id>[a-z][a-z]\d+)\s*)(?:;(?<comment>.+))?$
-        const string PATTERN = "^(?<beforeComment>\\s*(?:http://www\\.nicovideo\\.jp/watch/)?(?<id>[a-z][a-z]\\d+)\\s*)(?:;(?<comment>.+))?$";
+        // ^(?<beforeComment>\s*(?:http://www\.nicovideo\.jp/watch/)?(?<id>(?:[a-z][a-z])?\d+)\s*)(?:;(?<comment>.+))?$
+        const string PATTERN = "^(?<beforeComment>\\s*(?:http://www\\.nicovideo\\.jp/watch/)?(?<id>(?:[a-z][a-z])?\\d+)\\s*)(?:;(?<comment>.+))?$";
         string line;
 
         private OnelineVideoInfo(string line) {
