236db7cf5aa4e6758ba3340773178db614f505e3
Hiroki Murayama <hazychill@gmail.com>
2012-01-17 00:37:22 +0900

    UsersessionFactoryExplicit

---
+++ UsersessionFctoryExplicit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hazychill.NicoCacheDriver {
+    class Program {
+        static void Main(string[] args) {
+            string usersession;
+            if (args.Length >= 1) {
+                usersession = args[0];
+            }
+            else {
+                usersession = string.Empty;
+            }
+            Console.WriteLine(usersession);
+        }
+    }
+}
