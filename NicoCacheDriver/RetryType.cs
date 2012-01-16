b2493b3a1714956b33bdcf49fe941282d443a657
Hiroki Murayama <hazychill@gmail.com>
2012-01-17 00:58:50 +0900

    ソースファイル追加忘れ

---
+++ NicoCacheDriver/RetryType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hazychill.NicoCacheDriver {
+    public enum RetryType {
+        None = 0,
+        Immediate,
+        Queued
+    }
+}
